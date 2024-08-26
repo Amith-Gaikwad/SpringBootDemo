59651d7a62261dae89343bc0cab918f4fcf0a196
Amith-Gaikwad <60054351+Amith-Gaikwad@users.noreply.github.com>
2024-08-26 09:38:11 +0530

    Add files via upload

---
+++ sbe1/src/main/java/com/avenger/sbe1/Sbe1Application.java
@@ -0,0 +1,13 @@
+package com.avenger.sbe1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbe1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbe1Application.class, args);
+    }
+
+}
---
+++ sbe1/src/test/java/com/avenger/sbe1/Sbe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avenger.sbe1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbe1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
